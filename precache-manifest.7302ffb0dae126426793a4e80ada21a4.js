3fe3ff9e89ad087c626dde4a18a97549f2ea1f1d
Ayush Sinha <toayushofficial@gmail.com>
2020-04-25 01:26:52 +0530

    Updates

---
+++ precache-manifest.7302ffb0dae126426793a4e80ada21a4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5871e9d23b074c4df1c328dcb5e89821",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2727f75da5e59131884e",
+    "url": "/monsters-rolodex/static/css/main.c08a9832.chunk.css"
+  },
+  {
+    "revision": "cc86f92a0e9b1ed7b6f7",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.1b35cb52.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2727f75da5e59131884e",
+    "url": "/monsters-rolodex/static/js/main.ad4a834e.chunk.js"
+  },
+  {
+    "revision": "ac7692f361dea471e0d6",
+    "url": "/monsters-rolodex/static/js/runtime-main.ab61569e.js"
+  }
+]);
